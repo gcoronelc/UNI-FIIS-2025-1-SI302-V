203001fa007307503a41a7fd7cb2743408605422
Gustavo Coronel <gcoronelc@gmail.com>
2025-04-08 12:05:19 -0500

    Semana 4

---
+++ Semana04-08ABR/Problema14/src/main/java/pe/edu/uni/problema14/Problema14.java
@@ -0,0 +1,12 @@
+
+package pe.edu.uni.problema14;
+
+import pe.edu.uni.problema14.view.OperadorView;
+
+public class Problema14 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        OperadorView.main(args);
+    }
+}
